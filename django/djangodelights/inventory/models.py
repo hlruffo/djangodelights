8af18f17693beb7b3646c695b532bceb26198be7
Hugo Ledertheil Ruffo <hlruffo@gmail.com>
2023-02-16 19:09:18 -0300

    Ajustes na exibição do painel admin

--- django/djangodelights/inventory/models.py
+++ django/djangodelights/inventory/models.py
@@ -8,11 +8,17 @@ class Ingredients(models.Model):
     unit = models.CharField(max_length=3)
     ingredient_price = models.DecimalField(decimal_places=2, max_digits=3)
 
+    def __str__(self):
+        return self.name
+
 
 class MenuItem(models.Model):
     title = models.CharField(
         max_length=30, help_text="Enter Menu's Item Name")
     item_price = models.DecimalField(decimal_places=2, max_digits=3)
+
+    def __str__(self):
+        return self.title
 
 
 class RecipeRequirements(models.Model):
@@ -20,7 +26,13 @@ class RecipeRequirements(models.Model):
     ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
     quantity = models.DecimalField(decimal_places=2, max_digits=5)
 
+    def __str__(self):
+        return self.menu_item.title
+
 
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.menu_item.title
